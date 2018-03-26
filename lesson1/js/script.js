abaf444057ac4b2975fc0a1cb966b349db55d119
Irina Bondarenko <lavless13@mail.ru>
2018-03-26 05:06:07 +0300

    Простое задание

---
+++ lesson1/js/script.js
@@ -0,0 +1,18 @@
+var money = prompt("Ваш бюджет?", '');
+name = prompt("Название вашего магазина?", '');
+
+mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+};
+
+mainList.shopGoods[0] = prompt('Какой тип товаров будем продавать?', '');
+mainList.shopGoods[1] = prompt('Какой тип товаров будем продавать?', '');
+mainList.shopGoods[2] = prompt('Какой тип товаров будем продавать?', '');
+
+alert('Ваш бюджет на 1 день составит ' + mainList.budget / 30);
+
+console.log(mainList);
